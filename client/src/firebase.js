b3ccd1ed134cb6e6e556d76b0467a3eed293b3ac
onesirian <ldlockhart@gmail.com>
2026-02-01 21:28:48 -0600

    firebase security stuff

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjybTQ7Bv9HACWN-9ObuIW-KRw9bKIsgY",
-  authDomain: "gbot-core.firebaseapp.com",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: "gbot-core",
   storageBucket: "gbot-core.firebasestorage.app",
   messagingSenderId: "54671337536",
-  appId: "1:54671337536:web:1b64c9089d6043421e17a5"
+  appId: "1:54671337536:web:1b64c9089d6043421e17a5",
 };
 
 const app = initializeApp(firebaseConfig);
